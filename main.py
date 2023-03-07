3de4b112d372fcbdd8e7656c5eb84529ef83e642
Nadeem <m.nadeem83005@gmail.com>
2023-03-07 20:16:05 +0530

    remove base64

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 import logging
 import python_aternos as aternos
 import requests
-import base64
+
 # Clear shell
 os.system('cls')
 
@@ -73,12 +73,15 @@ async def restart(ctx):
         await ctx.reply("restarting the server")
     except:
         await ctx.reply("oops idk wat happened contact nadeem")
+
+
 @bot.command()
 async def ping(ctx):
     """
     Ping!
     """
     await ctx.reply('Pong!   `{0} seconds`'.format(round(bot.latency, 1)))
+
 
 @bot.command()
 @commands.has_permissions(administrator=True)
@@ -95,51 +98,53 @@ async def info(ctx):
     except:
         await ctx.reply("somethings wrong call nadeem")
 
+
 @bot.command(aliases=['status', 'Status'])
 async def mcstatus(ctx):
-        """
+    """
         MC Server Status, Updates every 10 minutes
         """
-        cur.fetch()
-        data = requests.get("https://eu.mc-api.net/v3/server/ping/Mincreft22.aternos.me:61553").json()
-        motdto = requests.get("https://mcapi.us/server/status?ip=Mincreft22.aternos.me&port=61553").json()
-        embed = discord.Embed(title="Server status", colour=discord.Colour.blurple())
-        try:
-            if cur.status == 'online':
-                embed.add_field(name="Status", value=f"{cur.status} :green_circle:")
-            else:
-                embed.add_field(name="Status" , value=f"{cur.status} :red_circle:")
-        except:
-            embed.add_field(name="Status", value="Failed")
-        try:
-            embed.add_field(name="Players online", value=f"{data['players']['online']} / {data['players']['max']}")
-        except:
-            embed.add_field(name="Players online", value="Failed")
-        try:
-            embed.add_field(name="Latency", value=f"{data['took']} ms")
-        except:
-            embed.add_field(name="Latency", value="Failed")
+    cur.fetch()
+    data = requests.get("https://eu.mc-api.net/v3/server/ping/Mincreft22.aternos.me:61553").json()
+    motdto = requests.get("https://mcapi.us/server/status?ip=Mincreft22.aternos.me&port=61553").json()
+    embed = discord.Embed(title="Server status", colour=discord.Colour.blurple())
+    try:
+        if cur.status == 'online':
+            embed.add_field(name="Status", value=f"{cur.status} :green_circle:")
+        else:
+            embed.add_field(name="Status", value=f"{cur.status} :red_circle:")
+    except:
+        embed.add_field(name="Status", value="Failed")
+    try:
+        embed.add_field(name="Players online", value=f"{data['players']['online']} / {data['players']['max']}")
+    except:
+        embed.add_field(name="Players online", value="Failed")
+    try:
+        embed.add_field(name="Latency", value=f"{data['took']} ms")
+    except:
+        embed.add_field(name="Latency", value="Failed")
 
-        try:
-            embed.add_field(name="Version", value=f"{cur.version}")
-        except:
-            embed.add_field(name="Version", value="Failed")
+    try:
+        embed.add_field(name="Version", value=f"{cur.version}")
+    except:
+        embed.add_field(name="Version", value="Failed")
 
-        try:
-            embed.add_field(name="Motd", value=f"{cur.motd}")
-        except:
-            embed.add_field(name="Motd", value="Failed")
+    try:
+        embed.add_field(name="Motd", value=f"{cur.motd}")
+    except:
+        embed.add_field(name="Motd", value="Failed")
 
-        try:
-            embed.add_field(name="Player Names", value=f"{motdto['players']['list']}")
-        except:
-            embed.add_field(name="Player Names", value="Failed")
+    try:
+        embed.add_field(name="Player Names", value=f"{motdto['players']['list']}")
+    except:
+        embed.add_field(name="Player Names", value="Failed")
 
-        try:
-            embed.set_thumbnail(url="https://images-ext-2.discordapp.net/external/or7mN4DQa7IqfuCuN8XTP3OQLlwx1hMTn0Cdq_qwWaM/https/eu.mc-api.net/v3/server/favicon/mincreft22.aternos.me%3A61553?width=80&height=80")
-        except:
-            embed.set_thumbnail(url="https://media.minecraftforum.net/attachments/300/619/636977108000120237.png")
-        await ctx.send(embed=embed)
+    try:
+        embed.set_thumbnail(
+            url="https://images-ext-2.discordapp.net/external/or7mN4DQa7IqfuCuN8XTP3OQLlwx1hMTn0Cdq_qwWaM/https/eu.mc-api.net/v3/server/favicon/mincreft22.aternos.me%3A61553?width=80&height=80")
+    except:
+        embed.set_thumbnail(url="https://media.minecraftforum.net/attachments/300/619/636977108000120237.png")
+    await ctx.send(embed=embed)
 
 
 bot.run(creds['tkn'], log_level=logging.ERROR, log_formatter=formater)
